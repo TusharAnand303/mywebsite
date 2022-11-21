0302d5bd4e4ee356055ed750b37c912385f1b34d
TusharAnand303 <tusharanand303@gmail.com>
2022-11-21 11:10:30 +0530

    Updates

---
+++ precache-manifest.c928bfe9495b971a35097f42f0f6c224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9bdbccf23f7e06917ef",
+    "url": "/mywebsite/static/css/main.e3bab8a8.chunk.css"
+  },
+  {
+    "revision": "f9bdbccf23f7e06917ef",
+    "url": "/mywebsite/static/js/main.f9bdbccf.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/mywebsite/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/mywebsite/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "ef5bf652825b4e7f70cc",
+    "url": "/mywebsite/static/js/runtime~main.ef5bf652.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mywebsite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "94d3c73bb5563bcbe218546386234c05",
+    "url": "/mywebsite/index.html"
+  }
+];
